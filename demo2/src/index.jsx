6518924968a41a75ed666f9adf2427ecf4a1b443
hcxowe <hcxowe@126.com>
2016-07-02 16:32:49 +0800

    keymaps

--- demo2/src/index.jsx
+++ demo2/src/index.jsx
@@ -4,3 +4,29 @@ import AnswerMulChoiceQuetion from './AnswerMulChoiceQuetion';
 
 var choices = ['question1', 'question2', 'question3', 'question4'];
 ReactDOM.render(<AnswerMulChoiceQuetion />, document.getElementById('content'));
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Test = React.createClass({
+    componentDidMount: function() {
+
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        
+    },
+    componentWillMount: function() {
+
+    },
+    setState({
+        :
+    });
+    render: function() {
+        return (
+            <div />
+        );
+    }
+
+});
+
+module.exports = Test;
---
+++ demo2/src/text.js
@@ -0,0 +1,8 @@
+/**
+ * [name description]
+ * @return {[type]} [description]
+ */
+var a = 2222;
+function name(){
+    a = 3;
+}
