3aae09b415e53d53db9c22f68678801701066a47
hcxowe <hcxowe@126.com>
2016-07-05 10:59:15 +0800

    finish demo3

---
+++ demo3/src/components/productTable.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ProductCategoryRow from './productCategoryRow';
+import ProductRow from './productRow';
+
+var ProductTable = React.createClass({
+    render : function(){
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product){
+            if(product.name.indexOf(this.props.filterText)===-1 || (!product.stocked && this.props.inStockOnly)){
+                return;
+            }
+
+            if(product.category != lastCategory){
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        }.bind(this));
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+});
+
+export default ProductTable;
