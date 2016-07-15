0ad22fdf7c98364b81f470f4e3bd18510ca1a885
hcxowe <hcxowe@126.com>
2016-07-15 18:23:20 +0800

    按需加载button、css模块

--- demo4/src/index.jsx
+++ demo4/src/index.jsx
@@ -1,5 +1,5 @@
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
